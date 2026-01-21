f93bb44880df61da6959bb202b9aed4128f94cbe
wh0am-i <you@example.com>
2026-01-21 09:48:51 -0300

    bugfixes

--- windows/train.py
+++ windows/train.py
@@ -3,12 +3,13 @@ from ultralytics import YOLO
 model = YOLO("../models/yolov8n.pt")
 
 model.train(
-    data="data.yaml",
-    epochs=50,
+    data="../data.yaml",
+    epochs=2,
     imgsz=640,
-    batch=16,
-    device=0,
+    batch=4,
+    device='cpu',
     workers=8,
     name="yolov8n_finetune",
-    pretrained=True
+    pretrained=True,
+    classes=[0, 1, 2, 3, 4]
 )
